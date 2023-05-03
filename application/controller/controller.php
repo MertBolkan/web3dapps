f036e289c00f519e8872ff5ca9c5ea5be64f081d
MertBolkan <mb849@sussex.ac.uk>
2023-05-03 22:13:56 +0100

    part b done

--- application/controller/controller.php
+++ application/controller/controller.php
@@ -7,21 +7,51 @@ class Controller {
 	public $model;
 	
 	// Create functions for the controller class
-	function __construct() // contructor of the class
+	function __construct($pageURI = null) // constructor of the class
 	{
-		// Create new objects for Load and Model
 		$this->load = new Load(); 
 		$this->model = new Model();
-		// Determine what page you are on
-		$this->home();
+		// determine what page you are on
+		$this->$pageURI();
 	}
     // home page function
 	function home()
 	{
-		// Get data from the defined model method - model3D_info()
 		$data = $this->model->model3D_info();
-		// Tell the loader what view to load and which data to use
 		$this->load->view('view3DAppTest_2', $data);
 	}
+
+	function apiCreateTable()
+	{
+	  	// echo "Create table function";
+		$data = $this->model->dbCreateTable();
+		$this->load->view('viewMessage', $data);
+	}
+	function apiInsertData()
+	{
+		$data = $this->model->dbInsertData();
+	   	$this->load->view('viewMessage', $data);
+	}  
+	function apiGetData()
+	{
+		$data = $this->model->dbGetData();
+		$this->load->view('view3DAppData', $data);
+	}  
+	
+	function dbCreateTable()
+	{
+		echo "Create Table Function";
+	}
+
+	function dbInsertData()
+	{
+		echo "Data Insert Function";
+	}
+
+	function dbGetData()
+	{
+		echo "Data Read Function";
+	}
+
 }
 ?>    
